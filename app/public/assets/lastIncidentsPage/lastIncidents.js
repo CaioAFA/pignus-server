75de630ab1453123d0adc42eb880414a5899f056
RSmiiiith <leandrosheyk@hotmail.com>
2020-10-07 15:00:11 -0300

    Resolvido problema dos backgrounds de algumas paginas e adicionada efeito de filtro na pagina de ultimos incidentes.

--- app/public/assets/lastIncidentsPage/lastIncidents.js
+++ app/public/assets/lastIncidentsPage/lastIncidents.js
@@ -1,8 +1,10 @@
 function closeIncidentInformation(){
-	document.getElementById("magnifiedImageDiv").style.display = "none";			
+	document.getElementById("magnifiedImageDiv").style.display = "none";
+	document.getElementById("main").style.filter = "blur(0px)";			
 }
 
 function openIncidentInformation(image){
 	document.getElementById("magnifiedImage").src = image.src;
 	document.getElementById("magnifiedImageDiv").style.display = "block";
+	document.getElementById("main").style.filter = "blur(8px)";
 }
